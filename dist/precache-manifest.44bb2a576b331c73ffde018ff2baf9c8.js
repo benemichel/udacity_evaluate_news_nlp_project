93d25db2ed3398b39434eb2401268c1ab5f517d9
betabernie <benediktmichel@gmx.de>
2020-12-10 10:57:51 +0100

    only for proof, added dist dir

---
+++ dist/precache-manifest.44bb2a576b331c73ffde018ff2baf9c8.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ea4004fd8db814d04e3b5bd16f384",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3030feda1a95e899a481",
+    "url": "main.css"
+  },
+  {
+    "revision": "3030feda1a95e899a481",
+    "url": "main.js"
+  }
+]);
